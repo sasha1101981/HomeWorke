b0fd8df4b2baede6358e707d1910b3c1e5736e28
sasha1101981 <124509399+sasha1101981@users.noreply.github.com>
2023-02-25 00:41:09 +0700

    HW12

---
+++ Seminar№2/HW06.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+//  обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+//Первый способ 
+// System.Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+//     if (a == 7)
+//     {
+//         System.Console.WriteLine("Выходной Воскресенье");
+//     }
+//     else if (a == 6)
+//     {
+//         System.Console.WriteLine("Выходной субота");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("Рабочий день");
+
+//Второй способ
+System.Console.WriteLine("Введите день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 6:
+        {
+            System.Console.WriteLine("Выходной Суббота");
+            break;
+        }
+    case 7:
+        {
+            System.Console.WriteLine("Выходной Воскресенье");
+            break;
+        }
+    default:
+        {
+            System.Console.WriteLine("Рабочий день");
+            break;
+        }
+}
---
+++ Seminar№2/HW07.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+//  или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+System.Console.WriteLine("Введимте число");
+string? number = Console.ReadLine();
+if (number!.Length < 4 )
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+     else
+{
+    System.Console.WriteLine(number[3]);
+}
