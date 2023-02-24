b15aa97955bcc4f2fc8ed3a4bc2cac8f3682f2ca
sasha1101981 <124509399+sasha1101981@users.noreply.github.com>
2023-02-25 00:40:40 +0700

    HW12

---
+++ Seminar№2/HW05.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+System.Console.WriteLine("Введите число");
+string? number = Console.ReadLine();
+if (number!.Length < 3 )
+{
+    System.Console.WriteLine("Несодержит третье число");
+}
+else
+{
+    System.Console.WriteLine(number[2]);
+}
